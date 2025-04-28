b3a01728a4cef4e8df9d74aec7c59cad8fe28a69
Pierre-Marie Garderes <pmgarderes@yahoo.fr>
2025-04-28 00:27:52 -0700

    added functionalities and counter, and instrucitons in the GUI

--- Wrapper_Process_Cilias.m
+++ Wrapper_Process_Cilias.m
@@ -5,23 +5,20 @@
   
 
 %% Prepare a bacth of reduced files: 
-foldername = 'D:\NIkon SoRA\IH25bis_VASP exp2-3_11-10-23' ; % 'D:\Image Coralie\IH19_pVASP_test2_7-24-23\SelectedND2\';
-DSfactor = 25;
-batch_downsample_nd2_folder(foldername, DSfactor, true);
+% foldername = 'D:\NIkon SoRA\IH25bis_VASP exp2-3_11-10-23' ; % 'D:\Image Coralie\IH19_pVASP_test2_7-24-23\SelectedND2\';
+% DSfactor = 25;
+% batch_downsample_nd2_folder(foldername, DSfactor, true);
 
 %% file to be processed
-% example>> filename  = 'D:\NIkon SoRA\IH25bis_VASP exp2-3_11-10-23\79381_#11_IH25_s1_x60_PVNl-low_HOECHST-pVASP-mScarlet-ADCY3.nd2';
-filename = 'D:\NIkon SoRA\IH25bis_VASP exp2-3_11-10-23\reduced_stack\79381_#11_IH25_s1_x60_PVNl-low_HOECHST-pVASP-mScarlet-ADCY3_reduced.mat';% 'C:\Users\calex\Box\Nikon SoRa_Coralie\IH19_pVASP_test2_7-24-23\tiff\MAX\MAX_76467_IH19_s2_x60_PVNli_HEOCHST-pVASP-mScarlet-ADCY3.tif';
+filename  = 'C:\Data\Images_Coralie\74617_Sim1creMC4RgfpOb+-AAVmScarlet-ARL13b-IH4s1x60PVli.nd2';
+% filename = 'D:\NIkon SoRA\IH25bis_VASP exp2-3_11-10-23\reduced_stack\79381_#11_IH25_s1_x60_PVNl-low_HOECHST-pVASP-mScarlet-ADCY3_reduced.mat';% 'C:\Users\calex\Box\Nikon SoRa_Coralie\IH19_pVASP_test2_7-24-23\tiff\MAX\MAX_76467_IH19_s2_x60_PVNli_HEOCHST-pVASP-mScarlet-ADCY3.tif';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %% -- List of all parameters  --
 % Downsampling factor for averaging the Z-stack 
 params.load_reduced =1; 
 params.DSfactor = 25;  % 25
 params.reload_previous_Detection = 0; % 1 or 0 % use 0 to overwrite previous detection
-
-
 
 % parameters for the GUI (ROI detection) 
 params.windowSize = 100;  % Size of the ROI window
@@ -44,7 +41,6 @@ tic
 % first load the images, downsampled by averaging  
 if params.load_reduced==0 
     [imgStack, metadata] = load_nd2_image_downsampled(filename, params);   else ;    load(filename) ; end
-
 if params.reload_previous_Detection 
     uniqueDetections = load_cilia_detections(filename, ciliaDetections, uniqueDetections);   else;     uniqueDetections = [];  end
 toc
